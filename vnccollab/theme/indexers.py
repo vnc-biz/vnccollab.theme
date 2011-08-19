dd862ca519aa6604003021888c553b9bea90eab5
Vitaliy Podoba <vitaliypodoba@gmail.com>
2011-08-19 13:52:58 +0000

    fix avg_ratings index

--- vnccollab/theme/indexers.py
+++ vnccollab/theme/indexers.py
@@ -2,16 +2,17 @@ from plone.indexer.decorator import indexer
 from zope.annotation.interfaces import IAnnotations
 
 from cioppino.twothumbs.interfaces import ILoveThumbsDontYou
-from cioppino.twothumbs.rate import yays, nays, getTotalPositiveRatings, \
-    getTally
+from cioppino.twothumbs.rate import yays, nays, getTotalPositiveRatings
 
 
 @indexer(ILoveThumbsDontYou)
 def avg_ratings(object, **kw):
     """Average rating: difference between thumb ups and downs.
     """
-    total = getTally(object)
-    return total['ups'] - total['downs']
+    annotations = IAnnotations(object)
+    ups = (yays in annotations) and len(annotations[yays]) or 0
+    downs = (nays in annotations) and len(annotations[nays]) or 0
+    return ups - downs
 
 @indexer(ILoveThumbsDontYou)
 def total_down_ratings(object, **kw):
