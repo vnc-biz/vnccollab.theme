4bd8cc77a566b96e137267cb18b6c8115bf7fd78
Vitaliy Podoba <vitaliypodoba@gmail.com>
2011-11-11 12:03:48 +0000

    implement login viewlet

--- vnccollab/theme/browser/viewlets.py
+++ vnccollab/theme/browser/viewlets.py
@@ -1,8 +1,11 @@
+from zope.component import getMultiAdapter
+
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.utils import safe_unicode, normalizeString
 
 from plone.app.layout.viewlets.common import ViewletBase
+from plone.memoize.instance import memoize
 
 from cioppino.twothumbs.rate import getTally
 from vnccollab.theme import messageFactory as _
@@ -49,3 +52,76 @@ class ActionsListViewlet(ViewletBase):
             if getattr(obj, 'portal_type', '') == 'ActionItemList':
                 self.todo = obj
                 break
+
+class LoginViewlet(ViewletBase):
+    """Most methods are copied over from login portlet renderer"""
+    
+    def __init__(self, *args, **kw):
+        super(LoginViewlet, self).__init__(*args, **kw)
+
+        self.membership = getToolByName(self.context, 'portal_membership')
+        self.context_state = getMultiAdapter((self.context, self.request),
+            name=u'plone_context_state')
+        self.portal_state = getMultiAdapter((self.context, self.request),
+            name=u'plone_portal_state')
+        self.pas_info = getMultiAdapter((self.context, self.request),
+            name=u'pas_info')
+
+    def show(self):
+        if not self.portal_state.anonymous():
+            return False
+        if not self.pas_info.hasLoginPasswordExtractor():
+            return False
+        page = self.request.get('URL', '').split('/')[-1]
+        return page not in ('login_form', '@@register')
+
+    @property
+    def available(self):
+        return self.auth() is not None and self.show()
+
+    def login_form(self):
+        return '%s/login_form' % self.portal_state.portal_url()
+
+    def mail_password_form(self):
+        return '%s/mail_password_form' % self.portal_state.portal_url()
+
+    def login_name(self):
+        auth = self.auth()
+        name = None
+        if auth is not None:
+            name = getattr(auth, 'name_cookie', None)
+        if not name:
+            name = '__ac_name'
+        return name
+
+    def login_password(self):
+        auth = self.auth()
+        passwd = None
+        if auth is not None:
+            passwd = getattr(auth, 'pw_cookie', None)
+        if not passwd:
+            passwd = '__ac_password'
+        return passwd
+
+    def join_action(self):
+        context = self.context
+        tool = getToolByName(context, 'portal_actions')
+        join = tool.listActionInfos(action_chain='user/join', object=context)
+        if len(join) > 0:
+            return join[0]['url']
+        return None
+
+    def can_register(self):
+        if getToolByName(self.context, 'portal_registration', None) is None:
+            return False
+        return self.membership.checkPermission('Add portal member',
+            self.context)
+
+    def can_request_password(self):
+        return self.membership.checkPermission('Mail forgotten password',
+            self.context)
+
+    @memoize
+    def auth(self, _marker=[]):
+        acl_users = getToolByName(self.context, 'acl_users')
+        return getattr(acl_users, 'credentials_cookie_auth', None)
