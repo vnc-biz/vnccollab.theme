a29e6c42ad1e36c18a9e9b9afca3489a4205d75b
Vitaliy Podoba <vitaliypodoba@gmail.com>
2011-11-11 13:16:24 +0000

    organize header viewlet manager, add links viewlet, came up with opened/closed states

--- vnccollab/theme/browser/interfaces.py
+++ vnccollab/theme/browser/interfaces.py
@@ -1,6 +1,10 @@
+from zope.viewlet.interfaces import IViewletManager
 from plone.theme.interfaces import IDefaultPloneLayer
 
 
 class IThemeSpecific(IDefaultPloneLayer):
     """Marker interface that defines a Zope 3 browser layer.
     """
+
+class IVNCCollabHtmlHead(IViewletManager):
+    """A viewlet manager that sits right after portal tabs viewlet"""
