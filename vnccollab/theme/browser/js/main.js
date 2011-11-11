26152dac0be5047f10b2647220897e5120575bad
Vitaliy Podoba <vitaliypodoba@gmail.com>
2011-11-11 14:36:55 +0000

    add js handler for header area

---
+++ vnccollab/theme/browser/js/main.js
@@ -0,0 +1,32 @@
+function attachHeaderViewletCloseOpen() {
+  jq('.headerTimeViewlet a.closeLink').click(function(event){
+    var container = jq(event.target).parents('#vnccollab-header');
+    if (container.length == 0) {
+      return false;
+    }
+    
+    if (container.is('.opened')) {
+      container.removeClass('opened').addClass('closed');
+      createCookie('vnccollab_header_state', 'closed', 365);
+    }
+    
+    return false;
+  });
+  jq('.headerTimeViewlet a.openLink').click(function(event){
+    var container = jq(event.target).parents('#vnccollab-header');
+    if (container.length == 0) {
+      return false;
+    }
+    
+    if (container.is('.closed')) {
+      container.removeClass('closed').addClass('opened');
+      createCookie('vnccollab_header_state', 'opened', 365);
+    }
+    
+    return false;
+  });
+}
+
+jq(function() {
+  attachHeaderViewletCloseOpen();
+});
