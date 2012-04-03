76ce2befb283a1243b7034666f83efc2f8f1a3a5
José Dinuncio <jose.dinuncio@gmail.com>
2012-04-03 18:25:55 +0200

    Isolate OpenERP portlet in iframa to avoid jQuery version conflict

---
+++ vnccollab/theme/browser/openerp.py
@@ -0,0 +1,8 @@
+from zope.interface import Interface, implements
+
+from Products.Five.browser import BrowserView
+
+
+
+class IframeHelperView(BrowserView):
+    """Helper view for OpenERP Portlet"""
