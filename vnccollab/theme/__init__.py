5ebc2efe54cdbf2ffab370f2bd43c84f38aa7a06
José Dinuncio <jose.dinuncio@gmail.com>
2012-05-19 13:30:54 -0430

    Monkey patching pytz to add Asia/mumbai

--- vnccollab/theme/__init__.py
+++ vnccollab/theme/__init__.py
@@ -5,3 +5,23 @@ messageFactory = MessageFactory('vnccollab.theme')
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
+
+def new_timezone(zone):
+    ''' Monkey patching replacement for pytz.timezone'''
+    zone = ZONE_MAP.get(zone, zone)
+    return original_timezone(zone)
+
+
+# Monkey patching pytz.timezone and pytz.commont_timezones
+
+print '*'*80
+print 'Patching pytz'
+ZONE_MAP = {'Asia/Mumbai': 'Asia/Kolkata',}
+
+import pytz
+original_timezone = pytz.timezone
+pytz.common_timezones.extend(ZONE_MAP.keys())
+pytz.common_timezones.sort()
+
+pytz.timezone = new_timezone
+print '*'*80
