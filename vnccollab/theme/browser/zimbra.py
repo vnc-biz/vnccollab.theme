032b499b6cabd89ed7a707ee51e7321254b02844
José Dinuncio <jose.dinuncio@gmail.com>
2012-06-04 19:16:36 -0430

    Refactors browser/zimbra.py to allow it to be used by Dashlet.

--- vnccollab/theme/browser/zimbra.py
+++ vnccollab/theme/browser/zimbra.py
@@ -33,19 +33,19 @@ def findMsgBody(node, format='text/html'):
             body = findMsgBody(sub_node, format)
             if body:
                 return body
-    
+
     return u''
 
 
 class ZimbraMailPortletView(BrowserView):
     """High level Zimbra Mail Portlet view to communicate with Zimbra SOAP API.
-    
+
     It uses pyzimbra and SOAPpy.
     """
-    
+
     _emails_template = ZopeTwoPageTemplateFile(
         'templates/zimbra_emails_template.pt')
-    
+
     def __init__(self, context, request):
         self.context = context
         self.request = request
@@ -56,44 +56,56 @@ class ZimbraMailPortletView(BrowserView):
 
     def __call__(self, action, portlethash):
         """Main method that accepts action to perform.
-        
+
         Args:
           @action - one of allowed actions, like emails, create, send, etc...
           @portlethash - portlet hash so we get portlet assignement data from it
-        
+
         This method accepts only post requests.
         It returns json.
         It authenticates before any further actions.
         """
         # get settings
-        self.data = data = self._data(portlethash)
-        
+        data = self._data(portlethash)
+
         # check if method is POST
         request = self.request
         if request.method != 'POST':
             return self._error(_(u"Request method is not allowed."))
-        
+
         # check if action is valid
         if action not in ('emails', 'email'):
             return self._error(_(u"Requested action is not allowed."))
-        
+
+        mails = self._execute_action(action, data, request)
+        return simplejson.dumps(mails)
+
+    def _execute_action(self, action, data, request={}):
         # create zimbra client and authenticate
+        self.data = data
         self.client = ZimbraClient('%s/service/soap' % data['url'])
         self._authenticate()
 
         # perform actual action
-        data = {}
+        mails = []
         if action == 'emails':
-            data = self.get_emails(request.get('folder') or None,
+            mails = self.get_emails(request.get('folder') or None,
+                int(request.get('offset') or '0'),
+                int(request.get('limit') or '100'),
+                request.get('recip') or '1',
+                request.get('sortBy') or 'dateDesc'
+            )
+        elif action == 'emails_as_dicts':
+            mails = self.get_emails_as_dicts(request.get('folder') or None,
                 int(request.get('offset') or '0'),
                 int(request.get('limit') or '100'),
                 request.get('recip') or '1',
                 request.get('sortBy') or 'dateDesc'
             )
         elif action == 'email':
-            data = self.get_email(request.get('eid') or None)
-        
-        return simplejson.dumps(data)
+            mails = self.get_email(request.get('eid') or None)
+
+        return mails
 
     def _error(self, msg):
         return simplejson.dumps({'error': msg})
@@ -103,11 +115,11 @@ class ZimbraMailPortletView(BrowserView):
         # TODO: do not login every request, but only on timing bases (e.g.
         #       every 10 minutes)
         self.client.authenticate(self.data['username'], self.data['password'])
-    
-    def get_emails(self, folder=None, offset=0, limit=10, recip='1',
+
+    def get_emails_as_dicts(self, folder=None, offset=0, limit=10, recip='1',
                    sortBy='dateDesc'):
-        """Returns list of email conversations.
-        
+        """Returns list of email conversations. Each email is represented by a dict.
+
         Args:
           @folder - if given, return list of emails from inside this folder
           @offset - if given, return list of emails starting from start
@@ -126,7 +138,7 @@ class ZimbraMailPortletView(BrowserView):
         if folder:
             query['query'] = 'in:%s' % folder
         result = self.client.invoke('urn:zimbraMail', 'SearchRequest', query)
-        
+
         # if result contains only one item then it won't be list
         if not isinstance(result, list):
             result = [result]
@@ -137,7 +149,7 @@ class ZimbraMailPortletView(BrowserView):
             people = item.e
             if not isinstance(people, list):
                 people = [people]
-            
+
             emails.append({
                 'subject': su(item.su),
                 'body': u'%s (%s) - %s - %s' % (u', '.join([p._getAttr('d')
@@ -147,26 +159,42 @@ class ZimbraMailPortletView(BrowserView):
                 'id': item._getAttr('_orig_id'),
                 'date': item._getAttr('d'),
             })
-        
+
+        return emails
+
+    def get_emails(self, folder=None, offset=0, limit=10, recip='1',
+                   sortBy='dateDesc'):
+        """Returns list of email conversations.
+
+        Args:
+          @folder - if given, return list of emails from inside this folder
+          @offset - if given, return list of emails starting from start
+          @limit - return 'limit' number of emails
+          @recip - whether to return 'to' email adress instead of 'from' for
+                   sent messages and conversations
+          @sort_by - sort result set by given field
+        """
+
+        emails = self.get_emails_as_dicts(folder, offset, limit, recip, sortBy)
         return {'emails': self._emails_template(emails=emails).encode('utf-8')}
 
     def get_email(self, eid):
         """Returns conversation emails by given id.
-        
+
         It also marks conversation as read.
         """
         if not eid:
             return {'error': _(u"Conversation id is not valid.")}
 
         # TODO: make zimbra conversation as read
-        
+
         result = self.client.invoke('urn:zimbraMail', 'GetConvRequest',
             {'c': {'id': eid, 'fetch': 'all', 'html': '1'}}).m
-            
+
         # if result contains only one item then it won't be list
         if not isinstance(result, list):
             result = [result]
-        
+
         thread = []
         for item in result:
             thread.append({
@@ -178,7 +206,7 @@ class ZimbraMailPortletView(BrowserView):
                 'id': item._getAttr('_orig_id'),
                 'date': item._getAttr('d'),
             })
-        
+
         return {'conversation': '<br />'.join([t['from']+': '+t['body']
             for t in thread])}
 
@@ -192,7 +220,7 @@ class ZimbraMailPortletView(BrowserView):
         """
         context = aq_inner(self.context)
         info = unhashPortletInfo(portlethash)
-        assignment = assignment_mapping_from_key(context, info['manager'],                                                                       
+        assignment = assignment_mapping_from_key(context, info['manager'],
             info['category'], info['key'])[info['name']]
         renderer = Renderer(context, self.request, self, None, assignment)
         username, password = renderer.getAuthCredentials()
