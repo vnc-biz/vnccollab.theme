996a96d2a481e487faa654401e898f2e68944e50
Vitaliy Podoba <vitaliypodoba@gmail.com>
2013-05-08 19:39:29 +0300

    basically done with followers and following lists, only need some pretty styling

--- vnccollab/theme/browser/following.py
+++ vnccollab/theme/browser/following.py
@@ -5,6 +5,8 @@ from AccessControl import getSecurityManager
 from zope.component import getUtility
 
 from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFCore.utils import getToolByName
 
 from ..interfaces import IFollowing
 
@@ -14,12 +16,18 @@ class FollowingView(BrowserView):
     functionality.
     """
 
+    _users_list_template = ViewPageTemplateFile('templates/followers_list.pt')
+
+    def _auth_user(self):
+        """Authenticated user id"""
+        return getSecurityManager().getUser().getId()
+
     def follow_user(self, user1, user2):
         """Subscribe user1 to user2.
 
         If user1 is None, get authenticated user id.
         """
-        user = user1 and user1 or getSecurityManager().getUser().getId()
+        user = user1 and user1 or self._auth_user()
         following = getUtility(IFollowing)
         following.subscribe(user, user2)
 
@@ -36,7 +44,7 @@ class FollowingView(BrowserView):
 
         If user1 is None, get authenticated user id.
         """
-        user = user1 and user1 or getSecurityManager().getUser().getId()
+        user = user1 and user1 or self._auth_user()
         following = getUtility(IFollowing)
         following.unsubscribe(user, user2)
 
@@ -55,7 +63,7 @@ class FollowingView(BrowserView):
 
         If no userid given, get authenticated user id.
         """
-        user = user2 and user2 or getSecurityManager().getUser().getId()
+        user = user2 and user2 or self._auth_user()
 
         # return nothing if users are equal
         if user == user1:
@@ -78,3 +86,75 @@ class FollowingView(BrowserView):
             }
 
         return button
+
+    def user_following(self, userid=None):
+        """Returns rendered view of users current user is following"""
+        auth = self._auth_user()
+        userid = userid and userid or auth
+        purl = getToolByName(self.context, 'portal_url')()
+        img = '%s/defaultUser.png' % purl
+        mtool = getToolByName(self.context, 'portal_membership')
+        acl_users = getToolByName(self.context, 'acl_users')
+        owner = acl_users.getUserById(userid)
+        owner_name = userid
+        if owner:
+            owner_name = owner.getProperty('fullname') or userid
+        following = getUtility(IFollowing)
+
+        users = []
+        for uid in following.get_followings(userid):
+            user = acl_users.getUserById(uid)
+
+            # prepare image url
+            portrait = mtool.getPersonalPortrait()
+            if portrait is not None:
+                img = portrait.absolute_url()
+
+            users.append({
+                'id': uid,
+                'name': user.getProperty('fullname') or uid,
+                'url': '%s/author/%s' % (purl, uid),
+                'img': img,
+                'following': following.is_following(auth, uid),
+                'show_button': auth != uid,
+            })
+
+        return self._users_list_template(title='%s is Following:' % owner_name,
+            back_url='%s/author/%s' % (purl, userid),
+            users=users)
+
+    def user_followers(self, userid=None):
+        """Returns rendered view of current user followers"""
+        auth = self._auth_user()
+        userid = userid and userid or auth
+        purl = getToolByName(self.context, 'portal_url')()
+        img = '%s/defaultUser.png' % purl
+        mtool = getToolByName(self.context, 'portal_membership')
+        acl_users = getToolByName(self.context, 'acl_users')
+        owner = acl_users.getUserById(userid)
+        owner_name = userid
+        if owner:
+            owner_name = owner.getProperty('fullname') or userid
+        following = getUtility(IFollowing)
+
+        users = []
+        for uid in following.get_followers(userid):
+            user = acl_users.getUserById(uid)
+
+            # prepare image url
+            portrait = mtool.getPersonalPortrait()
+            if portrait is not None:
+                img = portrait.absolute_url()
+
+            users.append({
+                'id': uid,
+                'name': user.getProperty('fullname') or uid,
+                'url': '%s/author/%s' % (purl, uid),
+                'img': img,
+                'following': following.is_following(auth, uid),
+                'show_button': auth != uid,
+            })
+
+        return self._users_list_template(title='%s Followers:' % owner_name,
+            back_url='%s/author/%s' % (purl, userid),
+            users=users)
