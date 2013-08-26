04452997fc6a6058dd598e09cc14ddaf7eae5c5f
Marcin Staniszczak <marcin@staniszczak.pl>
2013-08-26 19:37:39 +0200

    add dependencies for multi upload, change in upgrade step

--- vnccollab/theme/upgrades/upgrades.py
+++ vnccollab/theme/upgrades/upgrades.py
@@ -1,6 +1,8 @@
 from zope.component import getMultiAdapter
 from zope.publisher.browser import TestRequest
-#from Products.CMFCore.utils import getToolByName
+from Products.CMFCore.utils import getToolByName
+from plone.app.upgrade.utils import installOrReinstallProduct
+from plone import api
 
 from vnccollab.theme.config import PROJECTNAME
 
@@ -15,3 +17,13 @@ def upgrade_1103_1104(context):
                  'vnccollab.cloudstream.addcontentarea')
     manager.show('plone.portaltop',
                  'vnccollab.theme.addcontentarea')
+
+
+def upgrade_1108_1109(context):
+    setup = getToolByName(context, 'portal_setup')
+    setup.runImportStepFromProfile(DEFAULT_PROFILE, 'jsregistry',
+                                   run_dependencies=False)
+    setup.runImportStepFromProfile(DEFAULT_PROFILE, 'cssregistry',
+                                   run_dependencies=False)
+    portal = api.portal.get()
+    installOrReinstallProduct(portal, 'collective.quickupload')
