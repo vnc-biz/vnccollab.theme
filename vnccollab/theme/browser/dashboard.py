a7f5849f19168da84ebb7fc73f1650d8ee8f3e40
José Dinuncio <jose.dinuncio@gmail.com>
2013-10-16 18:24:05 -0430

    Adds four dashboard columns for Anonymous Group.

--- vnccollab/theme/browser/dashboard.py
+++ vnccollab/theme/browser/dashboard.py
@@ -4,6 +4,8 @@ from Products.Five.browser import BrowserView
 
 from plone.portlets.interfaces import IPortletManager
 from plone.portlets.constants import USER_CATEGORY, GROUP_CATEGORY
+from plone.app.portlets.browser.manage import \
+    ManageGroupDashboardPortlets as OriginalMGDPortlets
 
 from plone.memoize.instance import memoize
 
@@ -19,8 +21,10 @@ class DashboardView(BrowserView):
     @memoize
     def empty(self):
         dashboards = [getUtility(IPortletManager, name=name)
-            for name in ['plone.dashboard1', 'plone.dashboard2',
-            'plone.dashboard3', 'plone.dashboard4']]
+                      for name in ['plone.dashboard1',
+                                   'plone.dashboard2',
+                                   'plone.dashboard3',
+                                   'plone.dashboard4']]
 
         portal_membership = getToolByName(self.context, 'portal_membership')
         member = portal_membership.getAuthenticatedMember()
@@ -28,9 +32,38 @@ class DashboardView(BrowserView):
 
         num_portlets = 0
         for dashboard in dashboards:
-            num_portlets += len(dashboard.get(USER_CATEGORY, {}).get(userid,
-                {}))
+            num_portlets += len(dashboard.get(USER_CATEGORY, {})
+                                         .get(userid, {}))
             for groupid in member.getGroups():
-                num_portlets += len(dashboard.get(GROUP_CATEGORY,
-                    {}).get(groupid, {}))
+                num_portlets += len(dashboard.get(GROUP_CATEGORY, {})
+                                             .get(groupid, {}))
         return num_portlets == 0
+
+
+class ManageGroupDashboardPortlets(OriginalMGDPortlets):
+    """Manage Group Dashboard Portlets, customized for Anonymous Homepage."""
+    def getAssignmentsForManager(self, manager):
+        mapping = OriginalMGDPortlets.getAssignmentsForManager(self, manager)
+        if not mapping:
+            mapping = self.displaceMapping(manager)
+        return mapping
+
+    def displaceMapping(self, manager):
+        """Moves the assigned portlets from the next non empty portlet manager
+        to this one."""
+        next_manager = self._nextManager(manager)
+        if not next_manager:
+            # There's no next manager with portlets assigned
+            return []
+
+        mapping = OriginalMGDPortlets.getAssignmentsForManager(self,
+                next_manager)
+        # TODO: Assign to manager
+        # TODO: Delete from next_manager
+        # TODO: return mapping
+        return []
+
+    def _nextManager(self, manager):
+        """Returns the next non empty portlet manager."""
+        # TODO:
+        return None
