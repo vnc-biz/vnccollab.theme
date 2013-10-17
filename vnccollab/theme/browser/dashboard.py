ea15d87b6b5908448972ac55d0141d4d43904ca9
José Dinuncio <jose.dinuncio@gmail.com>
2013-10-17 09:28:39 -0430

    Removes ManageGroupDashboardPortlets.

--- vnccollab/theme/browser/dashboard.py
+++ vnccollab/theme/browser/dashboard.py
@@ -4,8 +4,6 @@ from Products.Five.browser import BrowserView
 
 from plone.portlets.interfaces import IPortletManager
 from plone.portlets.constants import USER_CATEGORY, GROUP_CATEGORY
-from plone.app.portlets.browser.manage import \
-    ManageGroupDashboardPortlets as OriginalMGDPortlets
 
 from plone.memoize.instance import memoize
 
@@ -38,32 +36,3 @@ class DashboardView(BrowserView):
                 num_portlets += len(dashboard.get(GROUP_CATEGORY, {})
                                              .get(groupid, {}))
         return num_portlets == 0
-
-
-class ManageGroupDashboardPortlets(OriginalMGDPortlets):
-    """Manage Group Dashboard Portlets, customized for Anonymous Homepage."""
-    def getAssignmentsForManager(self, manager):
-        mapping = OriginalMGDPortlets.getAssignmentsForManager(self, manager)
-        if not mapping:
-            mapping = self.displaceMapping(manager)
-        return mapping
-
-    def displaceMapping(self, manager):
-        """Moves the assigned portlets from the next non empty portlet manager
-        to this one."""
-        next_manager = self._nextManager(manager)
-        if not next_manager:
-            # There's no next manager with portlets assigned
-            return []
-
-        mapping = OriginalMGDPortlets.getAssignmentsForManager(self,
-                next_manager)
-        # TODO: Assign to manager
-        # TODO: Delete from next_manager
-        # TODO: return mapping
-        return []
-
-    def _nextManager(self, manager):
-        """Returns the next non empty portlet manager."""
-        # TODO:
-        return None
