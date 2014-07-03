dc0080c555f5ef94f67c1f86707a872fd41ad5ec
Johannes Hirte <johannes.hirte@vnc.biz>
2014-07-03 23:56:27 +0200

    correct file type

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='vnccollab.theme',
       version=version,
       description="An installable theme for VNC Collaboration Plone 4 Site.",
       long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+                       open(os.path.join("docs", "HISTORY.rst")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
